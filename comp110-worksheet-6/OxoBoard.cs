4d2b342c4c87fcf77bdae9dd0ca01b6052afe9aa
asdsadsa <asdsadsadasd>
2019-11-18 15:55:37 +0000

    Solved challenge not including stretch goal

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -10,38 +10,83 @@ namespace comp110_worksheet_6
 
 	public class OxoBoard
 	{
+
+        public int width;
+        public int height;
+
+        public Mark[,] board;
+
 		// Constructor. Perform any necessary data initialisation here.
 		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
 		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            width = 3;   //Set width and height of board, with a default value of 3x3
+            height = 3;  //
+            board = new Mark[width, height]; //Initialise the board
+
+            // Set all the board spaces to None
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    board[i, j] = Mark.None;
+                }
+            }
 		}
 
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            return board[x, y];
 		}
 
 		// If the specified square is currently empty, fill it with mark and return true.
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            if(board[x, y] == Mark.None)
+            {
+                board[x, y] = mark;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
 		}
 
 		// If there are still empty squares on the board, return false.
 		// If there are no empty squares, return true.
 		public bool IsBoardFull()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (board[i,j] == Mark.None)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
 
 		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
 		// Otherwise, return Mark.None.
 		public Mark GetWinner()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            string[] WinStates = {"001020", "011121", "021222", "000102", "101112", "202122", "001122", "201102"};
+            foreach (string winState in WinStates)
+            {
+                if (board[winState[0] - '0', winState[1] - '0'] == board[winState[2] - '0', winState[3] - '0'] && board[winState[2] - '0', winState[3] - '0']  == board[winState[4] - '0', winState[5] - '0'] && board[winState[4] - '0', winState[5] - '0'] != Mark.None)
+                {
+                    return board[winState[0] - '0', winState[1] - '0'];
+                }
+
+            }
+            return Mark.None;
 		}
 
 		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
@@ -50,12 +95,12 @@ namespace comp110_worksheet_6
 			for (int y = 0; y < 3; y++)
 			{
 				if (y > 0)
-					Console.WriteLine("--+---+--");
+					Console.WriteLine("──┼───┼──");
 
 				for (int x = 0; x < 3; x++)
 				{
 					if (x > 0)
-						Console.Write(" | ");
+						Console.Write(" │ ");
 
 					switch (GetSquare(x, y))
 					{
