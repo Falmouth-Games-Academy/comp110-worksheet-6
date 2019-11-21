4669463cb77129bce947b3f5f79675654cc73d7b
Bogdanoff <ChovekaZavur@gmail.com>
2019-11-21 16:02:16 +0000

    Implemented methods for a match 3 game.

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -6,71 +6,120 @@ using System.Threading.Tasks;
 
 namespace comp110_worksheet_6
 {
-	public enum Mark { None, O, X };
+    public enum Mark { None, O, X };
 
-	public class OxoBoard
-	{
-		// Constructor. Perform any necessary data initialisation here.
-		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
-		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+    public class OxoBoard
+    {
 
-		// Return the contents of the specified square.
-		public Mark GetSquare(int x, int y)
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+        public int width;
+        public int height;
 
-		// If the specified square is currently empty, fill it with mark and return true.
-		// If the square is not empty, leave it as-is and return False.
-		public bool SetSquare(int x, int y, Mark mark)
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+        public Mark[,] gameBoard;
+        // Constructor. Perform any necessary data initialisation here.
+        // Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
+        public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
+        {
+            width = height = 3;
+            gameBoard = new Mark[width, height];
 
-		// If there are still empty squares on the board, return false.
-		// If there are no empty squares, return true.
-		public bool IsBoardFull()
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    gameBoard[i, j] = Mark.None;
+                }
+            }
+        }
 
-		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
-		// Otherwise, return Mark.None.
-		public Mark GetWinner()
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+        // Return the contents of the specified square.
+        public Mark GetSquare(int x, int y)
+        {
+            return gameBoard[x, y];
+        }
 
-		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
-		public void PrintBoard()
-		{
-			for (int y = 0; y < 3; y++)
-			{
-				if (y > 0)
-					Console.WriteLine("--+---+--");
+        // If the specified square is currently empty, fill it with mark and return true.
+        // If the square is not empty, leave it as-is and return False.
+        public bool SetSquare(int x, int y, Mark mark)
+        {
+            if (gameBoard[x, y] == Mark.None)
+            {
+                gameBoard[x, y] = mark;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-				for (int x = 0; x < 3; x++)
-				{
-					if (x > 0)
-						Console.Write(" | ");
+        // If there are still empty squares on the board, return false.
+        // If there are no empty squares, return true.
+        public bool IsBoardFull()
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (gameBoard[i, j] == Mark.None)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
 
-					switch (GetSquare(x, y))
-					{
-						case Mark.None:
-							Console.Write(" "); break;
-						case Mark.O:
-							Console.Write("O"); break;
-						case Mark.X:
-							Console.Write("X"); break;
-					}
-				}
+        // If a player has three in a row, return Mark.O or Mark.X depending on which player.
+        // Otherwise, return Mark.None.
+        public Mark GetWinner()
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (gameBoard[i, j] != Mark.None) //Only look inside the cell if it's not empty.
+                    {
+                        if ((gameBoard[i, j] == gameBoard[i + 1, j]) && (gameBoard[i, j] == gameBoard[i + 2, j])) //Checks for a horizontal match on the board
+                        {
+                            return gameBoard[i, j];
+                        }
+                        else if ((gameBoard[i, j] == gameBoard[i, j + 1]) && (gameBoard[i, j] == gameBoard[i, j + 2])) //Check for a vertical match on the board
+                        {
+                            return gameBoard[i, j];
+                        }
+                    }
+                }
+            }
+            return Mark.None;
+        }
 
-				Console.WriteLine();
-			}
-		}
-	}
+        // Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
+        public void PrintBoard()
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                if (y > 0)
+                    Console.WriteLine("--+---+--");
+
+                for (int x = 0; x < 3; x++)
+                {
+                    if (x > 0)
+                        Console.Write(" | ");
+
+                    switch (GetSquare(x, y))
+                    {
+                        case Mark.None:
+                            Console.Write(" "); break;
+                        case Mark.O:
+                            Console.Write("O"); break;
+                        case Mark.X:
+                            Console.Write("X"); break;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
 }
 
