7a0b6679336dc9953d68615efc827cebbd56a75a
toby <ta228737@falmouth.ac.uk>
2019-11-21 18:02:02 +0000

    Edited OxoBoard with working Noughts and Crosses code.

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -8,41 +8,190 @@ namespace comp110_worksheet_6
 {
 	public enum Mark { None, O, X };
 
+    
 	public class OxoBoard
 	{
-		// Constructor. Perform any necessary data initialisation here.
-		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
-		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
+        public Mark[,] boardArray;
+
+        // Constructor. Perform any necessary data initialisation here.
+        // Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
+        public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            //throw new NotImplementedException("TODO: implement this function and then remove this exception");
+
+            boardArray = new Mark[3, 3];
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    boardArray[x, y] = Mark.None;
+                }
+            }
 		}
 
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+			//throw new NotImplementedException("TODO: implement this function and then remove this exception");
+
+            return boardArray[x, y];
+
+            // if cords are not on board
+
 		}
 
 		// If the specified square is currently empty, fill it with mark and return true.
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+
+            if((x >= 0 && x <= 2) && (y >= 0 && y <= 2))
+            {
+                if (boardArray[x, y] == Mark.None)
+                {
+                    boardArray[x, y] = mark;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            //throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            
+
+            
+
 		}
 
 		// If there are still empty squares on the board, return false.
 		// If there are no empty squares, return true.
 		public bool IsBoardFull()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+            //throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            int filledtiles = 0;
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if(boardArray[x , y] == Mark.None)
+                    {
+                        //empty
+                    }
+                    else
+                    {
+                        filledtiles++;
+                    }
+                }
+            }
+
+            if(filledtiles < 9)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
 		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
 		// Otherwise, return Mark.None.
 		public Mark GetWinner()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+			//throw new NotImplementedException("TODO: implement this function and then remove this exception");
+
+
+            // X left to right
+            if(boardArray[0,0] == Mark.X && boardArray[1, 0] == Mark.X && boardArray[2, 0] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (boardArray[0, 1] == Mark.X && boardArray[1, 1] == Mark.X && boardArray[2, 1] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (boardArray[0, 2] == Mark.X && boardArray[1, 2] == Mark.X && boardArray[2, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
+
+            // O left to right 
+            else if (boardArray[0, 0] == Mark.O && boardArray[1, 0] == Mark.O && boardArray[2, 0] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (boardArray[0, 1] == Mark.O && boardArray[1, 1] == Mark.O && boardArray[2, 1] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (boardArray[0, 2] == Mark.O && boardArray[1, 2] == Mark.O && boardArray[2, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+
+            // X Top to bottom
+
+            else if (boardArray[0, 0] == Mark.X && boardArray[0, 1] == Mark.X && boardArray[0, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (boardArray[1, 0] == Mark.X && boardArray[1, 1 ] == Mark.X && boardArray[1, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (boardArray[2, 0] == Mark.X && boardArray[2, 1] == Mark.X && boardArray[2, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
+
+            // O     Top to bottom
+
+            else if (boardArray[0, 0] == Mark.O && boardArray[0, 1] == Mark.O && boardArray[0, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (boardArray[1, 0] == Mark.O && boardArray[1, 1] == Mark.O && boardArray[1, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (boardArray[2, 0] == Mark.O && boardArray[2, 1] == Mark.O && boardArray[2, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+
+            //  Diagonal
+
+            else if (boardArray[0, 0] == Mark.X && boardArray[1, 1] == Mark.X && boardArray[2, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (boardArray[0, 2] == Mark.X && boardArray[1, 1] == Mark.X && boardArray[2, 0] == Mark.X)
+            {
+                return Mark.X;
+            }
+            // O Diagonal
+
+            else if (boardArray[0, 0] == Mark.O && boardArray[1, 1] == Mark.O && boardArray[2, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (boardArray[0, 2] == Mark.O && boardArray[1, 1] == Mark.O && boardArray[2, 0] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else
+            {
+                return Mark.None;
+            }
+
+
+        }
 
 		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
 		public void PrintBoard()
