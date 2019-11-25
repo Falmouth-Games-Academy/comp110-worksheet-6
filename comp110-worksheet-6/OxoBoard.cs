f931dd0c200086732f89b7d0d947a7c762c07f02
David <gamesdevdave@gmail.com>
2019-11-25 05:53:11 +0000

    Changed some of the code.

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -10,31 +10,53 @@ namespace comp110_worksheet_6
 
 	public class OxoBoard
 	{
+
+        private Mark[,] oxoArray;
+
 		// Constructor. Perform any necessary data initialisation here.
 		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
 		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            oxoArray = new Mark[3,3];
+            PrintBoard();
 		}
+
 
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            return oxoArray[x, y];
 		}
 
 		// If the specified square is currently empty, fill it with mark and return true.
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            if (oxoArray[x, y] == Mark.None)
+            {
+                oxoArray[x, y] = mark;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 		}
 
 		// If there are still empty squares on the board, return false.
 		// If there are no empty squares, return true.
 		public bool IsBoardFull()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            for(int i = 0, i < oxoArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < oxoArray.GetLength(1); j++)
+                {
+                    Mark currentMark = oxoArray[i, j];
+                    if (currentMark == Mark.None)
+                    {
+                        return false;
+                    }
+                }
 		}
 
 		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
