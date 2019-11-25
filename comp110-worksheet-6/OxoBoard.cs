44aaa5bd481c9e62b90464e719d0be5e68674bcb
Sam Hendrickx <samhendrickx93@gmail.com>
2019-11-25 13:44:30 +0100

    Completed assignment

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -7,42 +7,129 @@ using System.Threading.Tasks;
 namespace comp110_worksheet_6
 {
 	public enum Mark { None, O, X };
+    
 
 	public class OxoBoard
 	{
+        Mark[,] board;
 		// Constructor. Perform any necessary data initialisation here.
 		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
-		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
+		public OxoBoard(int width = 3, int height = 3, int inARow = 3)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+			board = new Mark[width,height];
 		}
 
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+			return board[x,y];
 		}
 
 		// If the specified square is currently empty, fill it with mark and return true.
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+			if(x < board.GetLength(0) && y < board.GetLength(1))
+                {
+                    if (board[x,y] == Mark.None)
+                    {
+                        board[x,y] = mark;
+                        return true;
+                    }                   
+                }
+            return false;
 		}
 
 		// If there are still empty squares on the board, return false.
 		// If there are no empty squares, return true.
 		public bool IsBoardFull()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+			for (int i = 0; i<board.GetLength(0); i++)
+                {
+                    for (int j = 0; j < board.GetLength(1); j++)
+                     {
+                        if (board[i,j] == Mark.None)
+                        {
+                            return false;
+                        }                        
+                     }
+               }
+            return true;
 		}
 
 		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
 		// Otherwise, return Mark.None.
 		public Mark GetWinner()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+			foreach(Mark mark in new Mark[] {Mark.O, Mark.X})
+                {
+                    if (DiagonalCheck1(mark) || DiagonalCheck2(mark))
+                    {
+                        return mark;
+                    }
+                    
+                    for (int i = 0; i < board.GetLength(0); i++)
+                    {
+                        if (HorizontalCheck(mark,i) || VerticalCheck(mark, i))
+                        {
+                            return mark;
+                        }
+                    }
+                }
+            return Mark.None;
+        }
+
+        //Check the horizontal Lines for the marks
+        public bool HorizontalCheck(Mark mark, int y)
+            {
+                for (int x = 0; x < board.GetLength(0); x++)
+                {
+                    if (board[x,y] != mark)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        //Check Vertical Lines for the marks
+        public bool VerticalCheck(Mark mark, int x)
+        {
+            for (int y = 0; y < board.GetLength(1); y++)
+            {
+                if (board[x, y] != mark)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //We need 2 checks for diagonals because of the different directions a diagonal win can take place
+        public bool DiagonalCheck1(Mark mark)
+            {
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    if(board[i,i] != mark)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        public bool DiagonalCheck2(Mark mark)
+            {
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    if (board[board.GetLength(0) - 1 -i, i] != mark)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
 
 		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
 		public void PrintBoard()
