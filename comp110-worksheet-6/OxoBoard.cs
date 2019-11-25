a9d5aaea12e2b0f611f92b834de7692101e11287
David <gamesdevdave@gmail.com>
2019-11-25 12:28:00 +0000

    Updated progress to worksheet.

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -10,16 +10,16 @@ namespace comp110_worksheet_6
 
 	public class OxoBoard
 	{
-
+        // Defines the array.
         private Mark[,] oxoArray;
 
 		// Constructor. Perform any necessary data initialisation here.
 		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
 		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
 		{
+            // Stores the board within an array.
             oxoArray = new Mark[3,3];
-            PrintBoard();
-		}
+        }
 
 
 		// Return the contents of the specified square.
@@ -32,6 +32,7 @@ namespace comp110_worksheet_6
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark)
 		{
+            // Checks a certain square dependent on x and y to see if it is empty or not.
             if (oxoArray[x, y] == Mark.None)
             {
                 oxoArray[x, y] = mark;
@@ -47,27 +48,92 @@ namespace comp110_worksheet_6
 		// If there are no empty squares, return true.
 		public bool IsBoardFull()
 		{
-            for(int i = 0, i < oxoArray.GetLength(0); i++)
+            int tilesUsed = 0;
+
+            // Checks every square on the board to see if there are any empty ones.
+            // If there aren't any, tilesUsed is used as a check.
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < oxoArray.GetLength(1); j++)
+                for (int j = 0; j < 3; j++)
                 {
                     Mark currentMark = oxoArray[i, j];
                     if (currentMark == Mark.None)
                     {
                         return false;
                     }
+                    else
+                    {
+                        tilesUsed++;
+                    }
                 }
+            }
+            // Check to see if there are any tiles left according to the variable tilesUsed.
+            if(tilesUsed < 9)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
 		}
 
-		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
-		// Otherwise, return Mark.None.
-		public Mark GetWinner()
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+        // If a player has three in a row, return Mark.O or Mark.X depending on which player.
+        // Otherwise, return Mark.None.
+        public Mark GetWinner()
+        {
+            if (oxoArray[0, 0] == Mark.X && oxoArray[1, 0] == Mark.X && oxoArray[2, 0] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (oxoArray[0, 0] == Mark.X && oxoArray[1, 1] == Mark.X && oxoArray[2, 1] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (oxoArray[0, 2] == Mark.X && oxoArray[1, 2] == Mark.X && oxoArray[2, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
 
-		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
-		public void PrintBoard()
+
+            else if (oxoArray[0, 0] == Mark.O && oxoArray[1, 0] == Mark.O && oxoArray[2, 0] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (oxoArray[0, 0] == Mark.O && oxoArray[1, 1] == Mark.O && oxoArray[2, 1] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (oxoArray[0, 2] == Mark.O && oxoArray[1, 2] == Mark.O && oxoArray[2, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+
+            else if (oxoArray[0, 0] == Mark.X && oxoArray[1, 1] == Mark.X && oxoArray[2, 2] == Mark.X)
+            {
+                return Mark.X;
+            }
+            else if (oxoArray[0, 2] == Mark.X && oxoArray[1, 1] == Mark.X && oxoArray[2, 0] == Mark.X)
+            {
+                return Mark.X;
+            }
+
+            else if (oxoArray[0, 0] == Mark.O && oxoArray[1, 1] == Mark.O && oxoArray[2, 2] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else if (oxoArray[0, 2] == Mark.O && oxoArray[1, 1] == Mark.O && oxoArray[2, 0] == Mark.O)
+            {
+                return Mark.O;
+            }
+            else
+            {
+                return Mark.None;
+            }
+        }
+
+            // Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
+            public void PrintBoard()
 		{
 			for (int y = 0; y < 3; y++)
 			{
