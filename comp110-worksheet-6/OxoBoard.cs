94f7a80bf5267806a4645f2056407433f72d413a
Quinny18 <Callum_Quinn1@outlook.com>
2019-11-25 03:18:39 +0000

    Completed worksheet (Not stretch goal)

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -11,37 +11,100 @@ namespace comp110_worksheet_6
 	public class OxoBoard
 	{
 		// Constructor. Perform any necessary data initialisation here.
+
+        Mark[,] GameBoard;
+
 		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
-		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
+		public OxoBoard(int width = 3, int height = 3, int inARow = 3)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            //Creating the board
+            GameBoard = new Mark[width, height];
 		}
 
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            return GameBoard[x, y];
 		}
 
 		// If the specified square is currently empty, fill it with mark and return true.
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark)
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            //Added validation for the users inputs
+            if (x < GameBoard.GetLongLength(0) && y < GameBoard.GetLongLength(1) && x > -1 && y > -1)
+            {
+                if (GameBoard[x, y] == Mark.None)
+                {
+                    GameBoard[x, y] = mark;
+                    return true;
+                }
+            } 
+            return false;
 		}
 
 		// If there are still empty squares on the board, return false.
 		// If there are no empty squares, return true.
 		public bool IsBoardFull()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            //iterating through the columns
+			for(int i = 0; i < GameBoard.GetLongLength(0); i++)
+            {
+                //iterating through the rows
+                for(int j = 0; j < GameBoard.GetLongLength(1); j++)
+                {
+                    //if the current position is empty, it will return false
+                    if (GameBoard[i, j] == Mark.None)
+                    {
+                        return false;
+                    }
+                    
+                }
+
+            }
+            //if the previous statements are not true, then it will return true
+            return true;
 		}
 
 		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
 		// Otherwise, return Mark.None.
 		public Mark GetWinner()
 		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+            //iterating through the rows
+			for (int horizontal = 0; horizontal < GameBoard.GetLongLength(0); horizontal++)
+            {
+                //checking if the contents of each position are the same for three in a row
+                if (GameBoard[horizontal, 0] == GameBoard[horizontal, 1] && GameBoard[horizontal, 1] == GameBoard[horizontal, 2])
+                {
+                    //checking if the tiles are player tiles or empty
+                    if(GetSquare(horizontal, 0) != Mark.None)
+                    {
+                        return GetSquare(horizontal, 0);
+                    }
+
+                }
+            }
+            //repeating the above process for the columns
+            for (int vertical = 0; vertical < GameBoard.GetLongLength(1); vertical++)
+            {
+                if (GameBoard[0, vertical] == GameBoard[1, vertical] && GameBoard[1, vertical] == GameBoard[2, vertical])
+                {
+                    if(GetSquare(0, vertical) != Mark.None)
+                    {
+                        return GetSquare(0 ,vertical);
+                    }
+                }
+            }
+            //checking on a diagonal
+            if (GameBoard[0, 0] == GameBoard[1, 1] && GameBoard[1, 1] == GameBoard[2, 2])
+            {
+                return GetSquare(0, 0);
+            }
+            if (GameBoard[2, 0] == GameBoard[1, 1] && GameBoard[1, 1] == GameBoard[0, 2])
+            {
+                return GetSquare(2, 0);
+            }
+            return Mark.None;
 		}
 
 		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
