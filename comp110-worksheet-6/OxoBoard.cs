f8337855563bb19399947a3b7aeca6f073a9fd2c
Alper Mimoglu <alpermimoglu@gmail.com>
2019-11-25 14:17:44 +0000

    finished i think?

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -8,24 +8,24 @@ namespace comp110_worksheet_6
 {
 	public enum Mark { None, O, X };
 
-	public class OxoBoard
-	{
-		// Constructor. Perform any necessary data initialisation here.
-		// Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
-		public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
-		{
-			int [,] tic-tack-toe = new int[3, 3];
-		}
+    public class OxoBoard
+    {
+        // Constructor. Perform any necessary data initialisation here.
+        // Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
+        public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */)
+        {
+            int[,] tictacktoe = new int[3, 3];
+        }
 
-		// Return the contents of the specified square.
-		public Mark GetSquare(int x, int y)
-		{
-			if (tic-tack-toe[x, y] == Mark.O)
+        // Return the contents of the specified square.
+        public Mark GetSquare(int x, int y)
+        {
+            if (tictacktoe[x, y] == Mark.O)
             {
                 return Mark.O;
 
             }
-            else if(tic-tack-toe[x, y] == Mark.X)
+            else if (tictacktoe[x, y] == Mark.X)
             {
                 return Mark.X;
             }
@@ -33,13 +33,13 @@ namespace comp110_worksheet_6
             {
                 return Mark.None;
             }
-		// If the specified square is currently empty, fill it with mark and return true.
-		// If the square is not empty, leave it as-is and return False.
-		public bool SetSquare(int x, int y, Mark mark)
-		{
-                if (tic-tack-toe[x, y] == Mark.None)
+            // If the specified square is currently empty, fill it with mark and return true.
+            // If the square is not empty, leave it as-is and return False.
+            public bool SetSquare(int x, int y, Mark mark)
+            {
+                if (tictacktoe[x, y] == Mark.None)
                 {
-                    tic-tack-toe[x, y] = mark;
+                    tictacktoe[x, y] = mark;
                     return true;
 
                 }
@@ -48,13 +48,13 @@ namespace comp110_worksheet_6
                     return false;
                 }
 
-		// If there are still empty squares on the board, return false.
-		// If there are no empty squares, return true.
-		public bool IsBoardFull()
-		{
-			for (int y= 0; y <2; y++)
+                // If there are still empty squares on the board, return false.
+                // If there are no empty squares, return true.
+                public bool IsBoardFull()
+                {
+                    for (int y = 0; y < 2; y++)
                     {
-                        for (int x = 0; x <2; x++)
+                        for (int x = 0; x < 2; x++)
                         {
                             if (board[x, y] == Mark.None)
                             {
@@ -62,14 +62,71 @@ namespace comp110_worksheet_6
                             }
                         }
                     }
-		}
+                }
 
-		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
-		// Otherwise, return Mark.None.
-		public Mark GetWinner()
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+                // If a player has three in a row, return Mark.O or Mark.X depending on which player.
+                // Otherwise, return Mark.None.
+                public Mark GetWinner()
+                {
+                    if Mark.O == tictacktoe[0, 0],[1, 1],[2, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit();
+                    }
+                    else if Mark.O == tictacktoe[1, 0],[1, 1],[0, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit();
+        
+                    else if Mark.O == tictacktoe[0, 0],[1, 0],[2, 0]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit(); 
+                    else if Mark.O == tictacktoe[0, 0] , [0, 1] , [0, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit(); 
+                    else if Mark.O == tictacktoe[1, 0] , [1, 1] , [1, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit(); 
+                    else if Mark.O == tictacktoe[2, 0] , [2, 1] , [2, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit();
+                    
+                    else if Mark.X == tictacktoe[0, 0],[1, 1],[2, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit();
+
+                    else if Mark.X == tictacktoe[1, 0],[1, 1],[0, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit();
+        
+                    else if Mark.X == tictacktoe[0, 0],[1, 0],[2, 0]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit(); 
+                    else if Mark.X == tictacktoe[0, 0] , [0, 1] , [0, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit(); 
+                    else if Mark.X == tictacktoe[1, 0] , [1, 1] , [1, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit(); 
+                    else if Mark.X == tictacktoe[2, 0] , [2, 1] , [2, 2]{
+                    Console.WriteLine("You Win");
+                    Console.ReadLine();
+                    Application.Exit();
+                    
+                   }
+              else Mark.None{
+               return false;
+    `         }
+		}           
 
 		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
 		public void PrintBoard()
