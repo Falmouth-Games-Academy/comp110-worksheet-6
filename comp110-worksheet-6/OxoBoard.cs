e50312650f72db441d925141692ee69bbd2e33eb
JB230713 <jb230713@falmouth.ac.uk>
2019-11-25 15:22:36 +0000

    -Added Comments

--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -12,9 +12,12 @@ namespace comp110_worksheet_6
 	{
         public Mark[,] gameBoard;
 
-
-        // Constructor. Perform any necessary data initialisation here.
-        // Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
+        /// <summary>
+        /// Using the height and width parameters to create the new board size.
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="inARow"></param>
         public OxoBoard(int width = 3, int height = 3, int inARow = 3)
 		{
             gameBoard = new Mark[width, height];
@@ -23,13 +26,21 @@ namespace comp110_worksheet_6
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y)
 		{
+            //getting the square that the player is inputing.
             return gameBoard[x, y];
         }
 
-		// If the specified square is currently empty, fill it with mark and return true.
-		// If the square is not empty, leave it as-is and return False.
-		public bool SetSquare(int x, int y, Mark mark)
+        /// <summary>
+        ///  If the specified square is currently empty, fill it with mark and return true
+        ///  If the square is not empty, leave it as-is and return False.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="mark"></param>
+        /// <returns></returns>
+        public bool SetSquare(int x, int y, Mark mark)
 		{	
+
             if(gameBoard[x,y] == Mark.None)
             {
                 gameBoard[x, y] = mark;
@@ -55,8 +66,12 @@ namespace comp110_worksheet_6
             return true;
 		}
 
-		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
-		// Otherwise, return Mark.None.
+		/// <summary>
+        /// Comparing all the filled spaces to eachother to check if there is a three in a row of any kind.
+        /// If there is then it will return the starting tile to select the winner, if there is no winner then
+        /// it will mark none.
+        /// </summary>
+        /// <returns></returns>
 		public Mark GetWinner()
 		{
             if(gameBoard[0,0] == gameBoard[1,0] && gameBoard[0,0] == gameBoard[2,0])
@@ -89,7 +104,7 @@ namespace comp110_worksheet_6
                 return gameBoard[2, 0];
             }
 
-            else if (gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[0, 0] == gameBoard[2, 1])
+            else if (gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[0, 0] == gameBoard[2, 2])
             {
                 return gameBoard[0, 0];
             }
@@ -106,8 +121,9 @@ namespace comp110_worksheet_6
         }
         
     
-
-		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
+        /// <summary>
+        /// using the x and y set this prints out the board into the console.
+        /// </summary>
 		public void PrintBoard()
 		{
 			for (int y = 0; y < 3; y++)
